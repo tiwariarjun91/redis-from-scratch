6c77f261aa40be96a91ea70684cc4c5cebf9859c
ArjunNandkishorTiwari <tiwari.ar@northeastern.edu>
2026-07-05 19:23:31 -0700

    Implement synchronous TCP echo server
    Signed-off-by: ArjunNandkishorTiwari <tiwari.ar@northeastern.edu>

--- src/com/dice/Main.java
+++ src/com/dice/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[ ] args)  throws IOException{
 
+        System.out.println("Rolling the dice let's go !!!");
+
         TCPServer server = new TCPServer();
 
         server.runSyncTCPServer();
--- src/com/dice/TCPServer.java
+++ src/com/dice/TCPServer.java
@@ -1,19 +1,21 @@
 package com.dice;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-class TCPServer {
+public class TCPServer {
 
     private static int PORT = 7379;
     private static String HOST = "0.0.0.0";
     private static int CONNECTIONS = 50;
+    private int concurrentClients = 0;
 
 
     public void runSyncTCPServer()  throws IOException{
-
-         System.out.println("Rolling the dice let's go !!!");
 
         ServerSocket serverSocket = new ServerSocket(
             PORT, 
@@ -22,5 +24,62 @@ class TCPServer {
 
         System.out.println("Listening on "+ HOST + ":" + PORT);
 
+        while(true){
+            Socket clientSocket = serverSocket.accept();
+
+            concurrentClients++;
+
+            System.out.println("Client connected: "
+                + clientSocket.getRemoteSocketAddress()
+                + " | concurrentClients = " 
+                + concurrentClients
+            );
+
+            while (true) {
+
+                String command  = readCommand(clientSocket);
+
+                System.out.println("Received command: " + command);
+
+                if(command == null || command.isEmpty()){
+                    concurrentClients--;
+
+                    System.out.println("Client disconnected: "
+                    + clientSocket.getRemoteSocketAddress()
+                    + " | concurrentClients = " 
+                    + concurrentClients
+                    );
+
+                    clientSocket.close();
+
+                    break;
+                }
+
+                writeOutPut(clientSocket, command);
+
+            }
+        }
+    }
+
+    private static String readCommand(Socket clienSocket) throws IOException {
+        InputStream inputStream = clienSocket.getInputStream();
+
+        byte[] buffer = new byte[1024];
+
+        int bytesRead = inputStream.read(buffer); //blocking
+
+        if (bytesRead == -1 ) {
+            return null;
+        }
+
+        return new String(buffer, 0 , bytesRead);
+    }
+
+    private static void writeOutPut(Socket clienSocket, String command) throws IOException {
+        OutputStream outputStream = clienSocket.getOutputStream();
+
+        outputStream.write(command.getBytes());
+
+        outputStream.flush();
     }
 }
